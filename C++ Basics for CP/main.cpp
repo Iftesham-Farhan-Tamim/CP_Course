aee3a13ce7d63b44070d2907ae176db6afc1f28b
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-30 21:04:12 +0600

    first commit

---
+++ C++ Basics for CP/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// freopen("input1.txt", "r", stdin);
+	// freopen("output1.txt", "w", stdout);
+
+	cout << sqrt(16) << endl;  // 4
+
+	// char c = 'a';
+	// char b = 'b';
+
+	// cout << c  << endl;  // a
+	// cout << (int) c << endl;  // 97
+	// cout << c - b << endl;  // -1 (ASCII Value)
+
+	// char z;
+	// cin >> z;
+	// cout << z << endl;  // cin -->9 cout-->9
+	// cout << (int) z << endl;  // 57(ASCII Value)
+
+	// char d;
+	// int a;
+	// cin >> d >> a;  // 12 51
+	// cout << d << " " << a << " ";  // 1 2 (char only store 1 int)
+
+	// double b = 3.5;
+	// cout << b << endl;  // 3.5
+
+	// bool d = false;
+	// cout << d << endl;  // flase --> 0, true--> all num except 0
+
+
+	// int a = 6;
+	// cout << a++ << endl;  // 6
+	// cout << a << endl;   // 7
+	// cout << a-- << endl; // 7
+	// cout << a << endl;  // 6
+
+	// cout << ++a << endl;  // 7
+	// cout << a << endl;  // 7
+	// cout << --a << endl;  // 6
+	// cout << a << endl;  // 6
+
+
+}
+
