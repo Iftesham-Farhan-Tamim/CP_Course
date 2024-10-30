13107fc2f9c19012e6236615662a929813bd8263
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-31 03:01:02 +0600

    second commit

---
+++ Conditions, Loops, Jump Statements and Questions/main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	// int a = 2;
+	// int b = 3;
+	// int c = 1;
+	// cout << (a == b) << endl;  // 0 (false)
+	// cout << (a <= b) << endl;  // 1 (true)
+	// cout << ((b > a) && (b > c)) << endl;  // 1 (true)
+	// cout << ((b > a) || (b > c)) << endl;  // 1 (true)
+
+	// Print patterns
+
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++) {
+	// 	int n;
+	// 	cin >> n;
+	// 	for (int j = 1; j <= n; j++) {
+	// 		for (int k = 1; k <= j; k++) {
+	// 			cout << "* ";
+	// 		}
+	// 		cout << endl;
+	// 	}
+	// }
+
+
+	// using while loop
+	// int t;
+	// cin >> t;
+	// while (t--) {
+	// 	int n;
+	// 	cin >> n;
+	// 	for (int i = 1; i <= n; i++) {
+	// 		for (int j = 1; j <= i; j++) {
+	// 			cout << "* ";
+	// 		}
+	// 		cout << endl;
+	// 	}
+	// }
+
+	// output:
+	// *
+	// * *
+	// * * *
+	// * * * *
+	// * * * * *
+	// *
+	// * *
+	// * * *
+
+
+	// while (true) {
+	// 	int n;
+	// 	cin >> n;
+	// 	if (n == 42) {
+	// 		break;
+	// 	}
+	// 	cout << n << endl;
+	// }
+
+
+	// sum of digits
+	// int t;
+	// cin >> t;
+	// for (int i = 0; i < t; i++) {
+	// 	int sum = 0;
+	// 	int n;
+	// 	cin >> n;
+	// 	while (n > 0) {
+	// 		sum += n % 10;
+	// 		n = n / 10;
+	// 	}
+	// 	cout << sum << endl;
+	// }
+
+
+	// using while loop
+	// int t;
+	// cin >> t;
+	// while (t--) {
+	// 	int sum = 0;
+	// 	int n;
+	// 	cin >> n;
+	// 	while (n > 0) {
+	// 		sum += n % 10;
+	// 		n = n / 10;
+	// 	}
+	// 	cout << sum << endl;
+	// }
+
+
+}
+ 
