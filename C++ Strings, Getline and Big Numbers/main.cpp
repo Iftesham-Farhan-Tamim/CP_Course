fe52aebb4c9dae736d268b557311f2be986a2c7a
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-11-08 11:10:02 +0600

    second commit

---
+++ C++ Strings, Getline and Big Numbers/main.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	// string str = "Hello";
+	// for (int i = 0; i < str.size(); i++) {
+	// 	cout << str[i] << endl;
+	// }
+
+
+	// string str1, str2, str3;
+	// cin >> str1 >> str2;
+	// cout << str1 << " " << str2;
+
+
+	// string str1, str2, str3, str4;
+	// getline(cin, str3);
+	// getline(cin, str4);
+	// cout << str3 << endl;
+	// cout << str4;
+
+
+	// int t;
+	// cin >> t;
+	// cin.ignore();
+	// while (t--) {
+	// 	string s;
+	// 	getline(cin, s);
+	// 	cout << s << endl;
+	// }
+
+
+	// string str;
+	// cin >> str;
+	// string str_rev;
+
+
+	// for (int i = str.size() - 1; i >= 0 ; i--) {
+	// 	str_rev += str[i];
+	// }
+	// cout << str_rev;  // not preferred(string contatinating into char)
+
+
+	// for (int i = str.size() - 1; i >= 0; i--) {
+	// 	str_rev.push_back(str[i]);
+	// 	// cout << str[i] << endl;
+	// }
+	// // cout << str_rev;
+
+	// if (str == str_rev) {
+	// 	cout << "YES";
+	// } else {
+	// 	cout << "NO";
+	// }
+
+
+	// string s;
+	// cin >> s;
+	// int last_digit = s[s.size() - 1] - '0';
+	// cout << last_digit;
+
+
+
+	// Note:
+	// cin >> s; Reads a string input s.
+	// int last_digit = s[s.size() - 1] - '0';
+	// s[s.size() - 1] accesses the last character of the string.
+	// Subtracting '0' converts the character into its integer equivalent.
+	// For example, if the last character is '5', '5' - '0' gives 5.
+	// cout << last_digit; Outputs the last digit as an integer.
+
+
+	// int t;
+	// cin >> t;
+
+	// while (t--) {
+	// 	int n;
+	// 	cin >> n;
+	// 	int arr[n];
+	// 	int ans = 0;
+
+	// 	for (int i = 0; i < n; i++) {
+	// 		cin >> arr[i];
+	// 	}
+
+	// 	for (int i = 0; i < n; i++) {
+	// 		if (arr[i] > ans) {
+	// 			ans = arr[i];
+	// 		}
+	// 	}
+
+	// 	cout << ans;
+	// }
+
+}
